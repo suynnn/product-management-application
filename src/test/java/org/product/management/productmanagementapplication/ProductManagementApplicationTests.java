50068505e2ada327eeeb73fc5c4dd2c108b8555f
suynnn <suy00065@gmail.com>
2024-10-06 10:09:11 +0900

    Chore: 초기설정

---
+++ src/test/java/org/product/management/productmanagementapplication/ProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.product.management.productmanagementapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
